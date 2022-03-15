07e7fa424f336e6c4eba69cfe10280e9053557be
Andres Urrego <j.a.u.m.06@hotmail.com>
2022-03-15 15:33:20 -0500

    Se crea nuevo branch y se genera codigo para arquitectura mvp

---
+++ app/src/main/java/com/anncode/offersandcoupons/model/CouponInteractorImpl.kt
@@ -0,0 +1,12 @@
+package com.anncode.offersandcoupons.model
+
+import com.anncode.offersandcoupons.presenter.CouponPresenter
+
+class CouponInteractorImpl(var couponPresenter: CouponPresenter): CouponInteractor {
+
+    private var couponRepository: CouponRepository = CouponRepositoryImpl(couponPresenter)
+
+    override fun getCouponsAPI() {
+        couponRepository.getCouponsAPI()
+    }
+}
---
+++ app/src/main/java/com/anncode/offersandcoupons/view/CouponView.kt
@@ -0,0 +1,13 @@
+package com.anncode.offersandcoupons.view
+
+import com.anncode.offersandcoupons.model.Coupon
+
+interface CouponView {
+
+    // Vista
+    fun showCoupons(coupons: ArrayList<Coupon>?)
+
+    // Presenter
+    fun getCoupons()
+
+}
